25ed155f062b6ed75c4688665289d6db735c898d
Bert Barabas <bert.barabas@outlook.com>
2024-10-22 23:16:08 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# reflex_aggrid_cross_talk
+# Test case for reflex issue with cross talk between two AgGrid components
+
+Steps to reproduce issue:
+
+1. Install and run this reflex program
+2. In the browser click on a column in Grid1
+3. Notice that the correct message is displayed below the grid
+4. Switch to Grid2 using the drop down (message is cleared)
+5. Notice that although there is no on_cell_clicked function defined for grid2, we still get the message printed when we click on a cell!
+6. Uncomment the on_cell_clicked line for grid2
+7. Notice that now that each grid has an on_cell_clicked function provided, that it does call the correct one.
---
+++ reflex_aggrid_cross_talk/reflex_aggrid_cross_talk.py
@@ -0,0 +1,82 @@
+import reflex as rx
+from reflex_ag_grid import ag_grid
+
+
+class State(rx.State):
+    """The app state."""
+
+    current_grid = "Grid1"
+    message = ""
+
+    def change_current_grid(self, new_grid: str):
+        self.message = ""
+        self.current_grid = new_grid
+
+    def grid1_cell_clicked(self):
+        self.message = "This should only print if a cell on Grid 1 was clicked!"
+
+    def grid2_cell_clicked(self):
+        self.message = "This should only print if a cell on Grid 2 was clicked!"
+
+
+def index() -> rx.Component:
+    return rx.container(
+        rx.vstack(
+            rx.heading("Testcase for AgGrid Crosstalk", size="9"),
+            rx.select(
+                ["Grid1", "Grid2"],
+                default_value=State.current_grid,
+                on_change=State.change_current_grid,
+            ),
+            rx.match(
+                State.current_grid,
+                (
+                    "Grid1",
+                    rx.vstack(
+                        rx.heading("Grid 1"),
+                        ag_grid(
+                            id="AgGrid1",
+                            column_defs=[
+                                ag_grid.column_def(field="Column 1"),
+                                ag_grid.column_def(field="Column 2"),
+                            ],
+                            row_data=[
+                                {"Column 1": "A1:1", "Column 2": "1:2"},
+                                {"Column 1": "A2:1", "Column 2": "2:2"},
+                                {"Column 1": "A3:1", "Column 2": "3:2"},
+                            ],
+                            on_cell_clicked=State.grid1_cell_clicked(),
+                        ),
+                    ),
+                ),
+                (
+                    "Grid2",
+                    rx.vstack(
+                        rx.heading("Grid 2"),
+                        ag_grid(
+                            id="AgGrid2",
+                            column_defs=[
+                                ag_grid.column_def(field="Column 1"),
+                                ag_grid.column_def(field="Column 2"),
+                            ],
+                            row_data=[
+                                {"Column 1": "B1:1", "Column 2": "1:2"},
+                                {"Column 1": "B2:1", "Column 2": "2:2"},
+                                {"Column 1": "B3:1", "Column 2": "3:2"},
+                            ],
+                            on_cell_clicked=State.grid2_cell_clicked(),
+                        ),
+                    ),
+                ),
+                rx.heading("This shouldn't happen"),
+            ),
+            rx.heading(f"message = {State.message}"),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+    )
+
+
+app = rx.App()
+app.add_page(index)
